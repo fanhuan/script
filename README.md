bbf3ee099a9217edefb738fd676428329c4c82c8
fanhuan <hfan22@wisc.edu>
2015-03-19 11:19:08 +0800

    with phylokmer function only

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ script
 ======
 
 version control of scripts
+
+###aaf_phylosim.py 
+is a variation of aaf_phylokmer.py to process simulated alignments from phylosim.
---
+++ aaf_phylosim.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  aaf_phylosim.py
+#  
+#  Copyright 2015 Huan Fan <hfan22@wisc.edu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
+import sys, gzip, bz2, os, time
+import multiprocessing as mp
+from optparse import OptionParser
+
+def smartopen(filename,*args,**kwargs):
+    if filename.endswith('gz'):
+        return gzip.open(filename,*args,**kwargs)
+    elif filename.endswith('bz2'):
+        return bz2.BZ2File(filename,*args,**kwargs)
+    else:
+        return open(filename,*args,**kwargs)
+
+def is_exe(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+
+usage = "usage: %prog [options]"
+version = '%prog 20150317.1'
+parser = OptionParser(usage = usage, version = version)
+parser.add_option("-k", dest = "kLen", type = int, default = 25, 
+                  help = "k-mer length, default = 25")
+parser.add_option("-o", dest = "outFile", default = 'phylokmer.dat',
+                  help = "output file, default = phylokmer.dat.gz")
+parser.add_option("-i", dest = "phyloSimFile",
+                  help = "the alignment file simulated by phylosim")
+parser.add_option("-G", dest = "memSize", type = int, default = 4,
+                  help = "total memory limit (in GB), default = 4")
+parser.add_option("-W", dest = "withKmer", action = 'store_true',
+                  help = "include k-mers in the shared k-mer table")
+
+(options, args) = parser.parse_args()
+    
+
+###check the input simfile:
+if not os.path.exists(options.phyloSimFile):
+    print 'Cannot find phyloSim file {}'.format(options.phyloSimFile)
+    sys.exit(2)
+
+
+###check for the executable files:
+#kmer_countx
+if options.kLen > 25:
+    if os.system('which kmer_countx > /dev/null'):
+        kmerCount = './kmer_countx'
+        if not is_exe(kmerCount):
+            print 'kmer_countx not found. Make sure it is in your PATH or the'
+            print 'current directory, and that it is executable'
+            sys.exit(1)
+    else:
+        kmerCount = 'kmer_countx'
+
+#kmer_count
+else:
+    if os.system('which kmer_count > /dev/null'):
+        kmerCount = './kmer_count'
+        if not is_exe(kmerCount):
+            print 'kmer_count not found. Make sure it is in your PATH or the'
+            print 'current directory, and that it is executable'
+            sys.exit(1)
+    else:
+        kmerCount = 'kmer_count'
+
+#kmer_merge
+if os.system('which kmer_merge > /dev/null'):
+    filt = './kmer_merge'
+    if not is_exe(filt):
+        print 'kmer_merge not found. Make sure it is in your PATH or the'
+        print 'current directory, and that it is executable'
+        sys.exit(1)
+else:
+    filt = 'kmer_merge'
+
+if os.path.exists(options.outFile):
+	s = raw_input('{} is already in your data directory, overwrite it? Y/N '
+                      .format(options.outFile))
+	if s == 'Y' or s == 'y':
+		print'{} is going to be overwritten'.format(options.outFile)
+	else:
+		print'No overwritting, exit'
+		sys.exit(2)
+
+###Get sample list and split alignment file into seperate fasta files for each node:
+dir_name = options.phyloSimFile.split('.')[0]
+os.system('mkdir {}'.format(dir_name))
+samples = []
+input=open(options.phyloSimFile)
+from Bio import SeqIO
+for seq_record in SeqIO.parse(input,"fasta"):
+	if seq_record.id != "Node":
+		samples.append(seq_record.id)
+		fh=open(dir_name+"/"+seq_record.id+".temp.fa","w")
+		fh.write(">"+seq_record.id+"\n")
+		fh.write(str(seq_record.seq))
+		fh.close()
+input.close()
+
+samples.sort()
+
+print 'SPECIES LIST:'
+for sample in samples:
+    print sample
+
+###Prepare kmer_count jobs. Here we use single core since the other cores will be used for parallele simulation
+jobList = []
+for sample in samples:
+	outFile = '{}/{}.pkdat'.format(dir_name,sample)
+	command = '{} -l {} -G {} -o {}'.format(kmerCount, options.kLen,
+			options.memSize, outFile)
+	if os.path.exists(dir_name+"/"+sample+".temp.fa"):
+		command += ' -i {}/{}.temp.fa'.format(dir_name, sample)
+	else:
+		print 'Error, file {}/{}.temp.fa does not exist. Aborting!'.\
+                   format(dir_name, sample)
+		sys.exit(3)
+	command += ' > {}/{}.wc'.format(dir_name,sample)
+	jobList.append(command)
+jobList = jobList[::-1]
+
+###Run jobs (1 thread)
+print time.strftime('%c')
+for job in jobList:
+	os.system(job)
+	print job
+
+###Merge output wc files
+divFile = os.path.join(dir_name, 'kmer_diversity.wc')
+handle = open(divFile, 'w')
+for sample in samples:
+	kmerFile = dir_name + "/" + sample + '.wc'
+	fh = open(kmerFile)
+	kmerNum = fh.readline().split(":")[1]
+	handle.write(sample + ":" + kmerNum)
+	fh.close()
+	os.remove(kmerFile)
+handle.close()
+
+###Run kmer_merge
+outFile = os.path.join(dir_name, options.outFile)
+handle = smartopen(outFile, 'w')
+print >> handle, '#-k {}'.format(options.kLen)
+for i, sample in enumerate(samples):
+    print >> handle, '#sample{}: {}'.format(i + 1, sample)
+handle.close()
+
+command = "{} -k s -c -d '0' -a 'T,M,F'".format(filt)
+cut = []
+for i, sample in enumerate(samples):
+    command += ' {}.pkdat'.format(os.path.join(dir_name, sample))
+    if options.withKmer:
+        cut.append('1')
+    cut.append(str((i + 1) * 2))
+if options.outFile.endswith('.gz'):
+    command += ' | cut -f {} | gzip >> {}'.format(','.join(cut), outFile)
+else:
+    command += ' | cut -f {} >> {}'.format(','.join(cut), outFile)
+print '\n', time.strftime('%c')
+print command
+os.system(command)
+print time.strftime('%c')
