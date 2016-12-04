094b311081122f3b871d900b4c1f08f59aa809ba
Huan Fan <hfan22@wisc.edu>
2016-12-04 10:04:30 -0600

    singletonCalculator.py

--- mapping.sh
+++ mapping.sh
@@ -1,13 +1,14 @@
+#$1 pattern name $2 mismatch $3 reference $4 shared kmer table
 #1. Get kmers from those top 10 rank patterns
-#python ~/GitHub/script/grep_pattern_from_kmer.py $2.dat.gz $1.pattern 1 | cut -f 1 > $1.kmer
+python ~/GitHub/script/grep_pattern_from_kmer.py $4 $1.pattern -t 3 
 #2. Turn kmer file into fasta file
 python ~/GitHub/script/kmer2fa.py $1.kmer
 #3. Map back to the reference genome
-if [ ! -f ~/Dropbox/Research/GWAS/Cohen/mapping/CP003248.2.fasta.amb ]; then
-  bwa index ~/Dropbox/Research/GWAS/Cohen/mapping/CP003248.2.fasta
+if [ ! -f $3.amb ]; then
+  bwa index $3
 fi
-bwa aln -n $2 ~/Dropbox/Research/GWAS/Cohen/mapping/CP003248.2.fasta $1.fa > $1.sai
-bwa samse -n 4 ~/Dropbox/Research/GWAS/Cohen/mapping/CP003248.2.fasta $1.sai $1.fa > $1_n$2.sam
+bwa aln -n $2 $3 $1.fa > $1.sai
+bwa samse -n 4 $3 $1.sai $1.fa > $1_n$2.sam
 #4. Convert sam to bam using samtools and sort it
 samtools view -bS $1_n$2.sam > $1.bam
 samtools sort $1.bam > sorted_$1.bam
