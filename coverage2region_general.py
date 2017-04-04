19901efe85eccb2bd4024008536ec605054ea921
fanhuan <hfan22@wisc.edu>
2017-04-04 16:30:03 -0500

    added coverage threshold

--- coverage2region_general.py
+++ coverage2region_general.py
@@ -6,17 +6,18 @@ import numpy as np
 def consecutive(data, stepsize=1):
     return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
-Usage = "%prog [options] coverage_file"
+Usage = "%prog [options] coverage_file threshold"
 version = '%prog 20170404.1'
 
 fh = open(sys.argv[1])
 prefix = os.path.basename(sys.argv[1])
 outfile = open('region' + prefix.lstrip('coverage'),'w')
+threshold = int(sys.argv[2])
 
 dic = {}
 for line in fh:
     line = line.split()
-    if int(line[-3]) > 1:
+    if int(line[-3]) > threshold:
         if line[0] in dic:
             dic[line[0]].append(int(line[-5]))
         else:
