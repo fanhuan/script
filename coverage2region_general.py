512c6c840045290eb418d2b983c0abcc47d90ca9
fanhuan <hfan22@wisc.edu>
2017-04-06 11:12:14 -0500

    coverage2region_general.py

--- coverage2region_general.py
+++ coverage2region_general.py
@@ -6,22 +6,22 @@ import numpy as np
 def consecutive(data, stepsize=1):
     return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
-Usage = "%prog [options] coverage_file threshold"
-version = '%prog 20170404.1'
+Usage = "%prog [options] coverage_file"
+version = '%prog 20170405.1'
 
 fh = open(sys.argv[1])
 prefix = os.path.basename(sys.argv[1])
 outfile = open('region' + prefix.lstrip('coverage'),'w')
-threshold = int(sys.argv[2])
-
+#threshold = int(sys.argv[2])
+coverage = []
 dic = {}
 for line in fh:
     line = line.split()
-    if int(line[-3]) > threshold:
-        if line[0] in dic:
-            dic[line[0]].append(int(line[-5]))
-        else:
-            dic[line[0]] = [int(line[-5])]
+    coverage.append(int(line[-3]))
+    if line[0] in dic:
+        dic[line[0]].append(int(line[-5]))
+    else:
+        dic[line[0]] = [int(line[-5])]
 
 for key in dic:
     for array in consecutive(dic[key]):
@@ -30,3 +30,4 @@ for key in dic:
 
 fh.close()
 outfile.close()
+print(np.mean(coverage),np.std(coverage))
