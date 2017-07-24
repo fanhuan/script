b462e583f8783ede458f7bb13c26b1bf075c3a0f
fanhuan <hfan22@wisc.edu>
2017-07-24 14:38:42 -0500

    trimFastq4mothur.py

--- README.md
+++ README.md
@@ -81,5 +81,8 @@ keeps stats of what was there, what’s left after random dropout and what has b
 ### subsample\_kmer_v2.py
 Some times we face a dataset with more than 1 order of magnitude differences in their counted kmer file. If the kmers were counted from genome assembly, there's nothing we could do about it. If the kmers were counted from raw data, especially metagenomic data, there is risk of insufficient sampling effort. A common practice in metagenomic analysis is to subsample other samples to the lowest acceptable sample and this script does it WITHOUT replacement in order to keep the same kmer diversity afterwards.  
 
+### trimFastq4mothur.py
+takes sanger sequencing reads and trim it based on quality score. If it is still longer than 1000bp after trimming, it will be trimmed from the end to make it shorter than 1000bp so mothur would take it for its makecontigs function.
+
 ### x2y.pl
 Universal converter of various formats in bioinformatics. See a detailed list at [Bio::SeqIO](http://bioperl.org/howtos/SeqIO_HOWTO.html).
---
+++ trimFastq4mothur.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#This is for subsampling
+'''
+from os.path import join, isfile, splitext
+from optparse import OptionParser
+import random
+import os, sys
+import numpy as np
+'''
+
+import sys,os
+import Bio
+
+Usage = "trimFastq4mothur.py fastq_directory"
+
+def trim_fastq_biopython(in_file, out_file, q_cutoff=10, consec=6, id=None):
+    """
+    Trim a FASTQ file and write out the trimmed sequences as a FASTQ file.
+
+    Only processes the sequence with identifer string rec.  If id
+    is None, takes first sequence.
+    """
+    # Load in sequences using Bio.SeqIO.  We'll keep the result as a dict.
+    sample = os.path.basename(in_file[:len(in_file)-len('*.fastq')])
+    with open(in_file) as f:
+        seqs = Bio.SeqIO.to_dict(Bio.SeqIO.parse(f, 'fastq'))
+
+    # Pull out the id we want
+    if id is None:
+        key, seq = seqs.popitem()
+    else:
+        try:
+            seq = seqs[id]
+        except KeyError:
+                raise KeyError('id not found in input file')
+
+    # Get Boolean array for good quality
+    q_good = np.array(seq.letter_annotations['phred_quality']) >= q_cutoff
+
+    # Find first set of consec good bases
+    i = 0
+    while i < len(q_good) - consec and not q_good[i:i+consec].all():
+        i += 1
+
+    # Find last set of consec good bases
+    j = len(q_good)
+    while j >= consec and not q_good[j-consec:j].all():
+        j -= 1
+    if j > 1000:
+        j = 1000
+    # Write out trimmed sequence
+
+    with open(out_file, 'w') as f:
+        seq.id = sample
+        seq.description = ''
+        Bio.SeqIO.write(seq[i:j], f, 'fastq')
+
+def main(fastq_dir):
+    for fileName in os.listdir(fastq_dir):
+        out_file = os.path.join(fastq_dir,'trimmed_' + fileName)
+        trim_fastq_biopython(os.path.join(fastq_dir,fileName), out_file, q_cutoff=10, consec=6, id=None):
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
