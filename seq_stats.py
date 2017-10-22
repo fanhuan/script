4810f43dcbc8e081068f0951df1a5cdf20860a74
Huan Fan <hfan22@wisc.edu>
2017-10-22 14:26:22 -0500

    dir option now working

--- seq_stats.py
+++ seq_stats.py
@@ -45,29 +45,30 @@ parser.add_option("-d", dest = "dir",
 		  help = "directory containing files")
 
 (options, args) = parser.parse_args()
-outfile = open(options.input.split('.')[0]+'_contig_length.txt','w')
-outfile.write('Contig\tLength\n')
+
 
 print('Sample\tNumSeq\tTotal_bp\tMean\tVar\tMin\tMax')
 
 if options.input:
     input_file = smartopen(options.input)
-    length=[]
-    for seq_record in SeqIO.parse(input_file,options.format):
-        outfile.write('%s\t%d\n'%(seq_record.id,len(seq_record.seq)))
-        length.append(len(seq_record.seq))
+	with open(options.input.split('.')[0]+'_contig_length.txt','w') as outfile:
+		outfile.write('Contig\tLength\n')
+	    length=[]
+	    for seq_record in SeqIO.parse(input_file,options.format):
+	        outfile.write('%s\t%d\n'%(seq_record.id,len(seq_record.seq)))
+	        length.append(len(seq_record.seq))
 	print(options.input,len(length),sum(length),np.mean(length),np.std(length), min(length),max(length))
-	input_file.close()
+		
 
 if options.dir:
     input_dir = options.dir
     for fileName in os.listdir(options.dir):
         input_handle = smartopen(options.dir+'/'+fileName)
-        length=[]
-        for seq_record in SeqIO.parse(input_handle,options.format):
-            outfile.write('%s\t%d\n'%(seq_record.id,len(seq_record.seq)))
-            length.append(len(seq_record.seq))
-        input_handle.close()
-    print(input_dir,len(length),sum(length),np.mean(length),np.std(length), min(length),max(length))
-
-outfile.close()
+		with open(options.dir+'/'+fileName.split('.')[0]+'_contig_length.txt','w') as outfile:
+			outfile.write('Contig\tLength\n')
+	        length=[]
+	        for seq_record in SeqIO.parse(input_handle,options.format):
+	            outfile.write('%s\t%d\n'%(seq_record.id,len(seq_record.seq)))
+	            length.append(len(seq_record.seq))
+	        input_handle.close()
+	    print(input_dir,len(length),sum(length),np.mean(length),np.std(length), min(length),max(length))
