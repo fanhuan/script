b770cf3cef30a9db9d2705185bced516217681a2
fanhuan <hfan22@wisc.edu>
2018-09-13 16:30:03 +0800

    for all assembly

--- contigtrimmer.py
+++ contigtrimmer.py
@@ -4,9 +4,7 @@
 #第一段：要用的模块函数之类的import
 from os.path import join, isfile, splitext
 from optparse import OptionParser
-import os
-import re
-import random
+from Bio import SeqIO
 #第二段：设定参数
 Usage = "contigtrimmer.py [ -i <input filename>][ -n <minimum contig length>]"
 version = '%prog 20180913.1'
@@ -17,28 +15,14 @@ parser.add_option( "-n", dest="mc",type=int, default=100, help="minimum contig l
 #第三段：传参
 iptf = open(options.iptf,'rU')
 mc=options.mc
-optf = open(splitext(options.iptf)[0]+'_'+str(mc),'w')
+optf = open(splitext(options.iptf)[0]+'_'+str(mc) + '.fa','w')
 
 
 #第四段：问题算法
 
-while True:
-    line = iptf.readline()
-    l=line.split('_') #tag那一行
-    if line.startswith('>'):
-        assert len(l) >= 2
-        if (int(l[3]))>=mc:
-            break
-        else:
-            pass
-    else:
-        pass
-while True:
-    if line=='':
-        break
-    else:
-        optf.write(line)
-        line = iptf.readline()
+for seq_record in SeqIO.parse(iptf,'fasta'):
+    if len(seq_record.seq) >= mc:
+        SeqIO.write(seq_record, optf,'fasta')
 
 iptf.close()
 optf.close()
