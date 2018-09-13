9d6b4383c2ce3208179e7699cf3311bd1e9080c4
fanhuan <hfan22@wisc.edu>
2018-09-13 15:54:33 +0800

    contigtrimmer for spades assembly

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ is a variation of aaf_phylokmer.py to process simulated alignments from phylosim
 ### aaf\_phylokmer_sba.py
 is a variation of aaf_phylokmer.py. It does everything aaf_phylokmer.py does plus generate a sh file containing the kmer_merge command for generating shared-by-all kmers.
 
+### contigtrimmer.py
+takes a spades assembly and trims it based on given minimum contig length
+
 ### count2hist.py
 takes .pkdat.gz and compute the hist file with the same prefix.
 
--- contigtrimmer.py
+++ contigtrimmer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 #-*- coding:utf-8 -*-
 # 去掉SOAPdonovo组装的短contig
 #第一段：要用的模块函数之类的import
@@ -9,6 +9,7 @@ import re
 import random
 #第二段：设定参数
 Usage = "contigtrimmer.py [ -i <input filename>][ -n <minimum contig length>]"
+version = '%prog 20180913.1'
 parser = OptionParser(Usage)
 parser.add_option( "-i", dest="iptf", help="input the fasta contig file")
 parser.add_option( "-n", dest="mc",type=int, default=100, help="minimum contig length")
@@ -23,10 +24,10 @@ optf = open(splitext(options.iptf)[0]+'_'+str(mc),'w')
 
 while True:
     line = iptf.readline()
-    l=line.split() #tag那一行
+    l=line.split('_') #tag那一行
     if line.startswith('>'):
         assert len(l) >= 2
-        if (int(l[2]))>=mc:
+        if (int(l[3]))>=mc:
             break
         else:
             pass
