b3b925cb792edf90ec9855529e2dc25a4deadcb3
fanhuan <hfan22@wisc.edu>
2018-09-13 13:54:27 +0800

    coverage2region

--- coverage2region_col2.py
+++ coverage2region_col2.py
@@ -12,16 +12,18 @@ def consecutive(data_list, stepsize=1):
 Usage = "%prog [options] mpileup_s_file <threshold>"
 version = '%prog 20180801.1'
 
-# positional argument
+# this assumes the mpileip file is only from one contig so no dictionary is needed
 
 threshold = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 
 prefix = os.path.basename(sys.argv[1])
-outfile = open('region' + '_' + prefix,'w')
+outfile = smartopen('region' + '_' + prefix,'w')
 #threshold = int(sys.argv[2])
-dic = {}
 coverage = []
 depth_list = []
+bp_list = []
+
+# positional argument
 with smartopen(sys.argv[1]) as fh:
     for line in fh:
         line = line.split()
@@ -32,18 +34,14 @@ with smartopen(sys.argv[1]) as fh:
         	    depth += int(line[i])
             except ValueError:
                 print(line,i,line[i])
-        	i += 4
+            i += 4
             if depth >= threshold:
         	    depth_list.append(depth)
-        	    if line[0] in dic:
-        	           dic[line[0]].append(int(line[1]))
-        	    else:
-        	           dic[line[0]] = [int(line[1])]
+        	    bp_list.append(int(line[1]))
 
-for key in dic:
-    for array in consecutive(dic[key]):
-        outfile.write('%s\t%d\t%d\t%d\t%s\n'%(key,array[0],array[-1],len(array),prefix.split('_')[1]))
-        coverage.append(len(array))
+for array in consecutive(bp_list):
+    outfile.write('%s\t%d\t%d\t%d\n'%(prefix.split('_')[1]),array[0],array[-1],len(array))
+    coverage.append(len(array))
 
 outfile.close()
 print('%s\t%f\t%f\t%f'%(prefix.split('_')[1],np.mean(depth_list),np.std(depth_list),sum(coverage)))
