b6a9d593bb91e8a8cab3ad36e0e01a8d9f9d5483
fanhuan <hfan22@wisc.edu>
2024-02-01 10:18:53 +0800

    count N in a genome

--- N_counter.py
+++ N_counter.py
@@ -16,14 +16,11 @@ def smartopen(filename,*args,**kwargs):
     else:
         return open(filename,*args,**kwargs)
         
-file_list = listdir(sys.argv[1])
-
 n=0
 total=0
-for seq_file in file_list:
-    if not seq_file.endswith("~"):
-        fh = smartopen(sys.argv[1]+'/'+seq_file)
-        for seq_record in SeqIO.parse(fh, "fastq-illumina"):
-            n += seq_record.seq.count('N')
-            total += len(seq_record.seq)
-print total, n, total - n
+fh = smartopen(sys.argv[1])
+
+for seq_record in SeqIO.parse(fh, "fasta"):
+    n += seq_record.seq.count('N')
+    total += len(seq_record.seq)
+print(total, n, total - n)
